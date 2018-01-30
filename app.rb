8219761f996a56b91556b8c2df2a5110abecf551
Justin Mecham <justin@mecham.me>
2018-01-30 13:53:30 -0500

    Added support for the Pingdom application key in the environment.

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ configure do
 
   set :email, ENV["PINGDOM_EMAIL"]
   set :password, ENV["PINGDOM_PASSWORD"]
-  set :app_key, ""
+  set :app_key, ENV["PINGDOM_APP_KEY"] || ""
   set :styles_path, "#{File.dirname(__FILE__)}/public/styles"
   set :scripts_path, "#{File.dirname(__FILE__)}/public/scripts"
   set :session_secret, ENV["SESSION_SECRET"] unless ENV["SESSION_SECRET"].nil?
@@ -31,15 +31,16 @@ get "/check/:check_id" do |check_id|
 
   app_key       = params[:app_key] || settings.app_key
   authorization = Base64.encode64("#{settings.email}:#{settings.password}")
+  headers       = { "App-Key" => app_key, "Authorization" => "Basic #{authorization}" }
 
   # Request Check Detail from Pingdom API
-  @check = open("https://api.pingdom.com/api/2.0/checks/#{check_id}", "App-Key" => app_key, "Authorization" => "Basic #{authorization}") do |response|
+  @check = open("https://api.pingdom.com/api/2.0/checks/#{check_id}", headers) do |response|
     JSON.parse(response.read)["check"]
   end
 
   if request.xhr?
     haml :check, :layout => false
-  else 
+  else
     haml :check
   end
 
